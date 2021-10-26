00bce6c606b4ac5a9f54295279b3e108eddacf52
Tyler Lafond <tlafo001@wch136-18.cs.ucr.edu>
2021-10-26 15:46:10 -0700

    Testing first project

--- Lab8_ADC/source/main.c
+++ Lab8_ADC/source/main.c
@@ -12,12 +12,36 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned short volt;
+unsigned char tempB;
+unsigned char tempD;
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	// 	in free Running Mode, a new conversion will trigger whenever
+	// 	the previous conversion completes
+}
+
+void TickADC() {
+	volt = ADC;
+	tempB = (char)volt;
+	tempD = (((char)(volt >> 8)) & 0x03);
+	PORTB = tempB;
+	PORTD = tempD;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
-    while (1) {
+	ADC_init();
 
-    }
-    return 1;
+	while (1) {
+		TickADC();
+	}
+	return 1;
 }
